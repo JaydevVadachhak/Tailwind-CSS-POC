7066471aee212bc95de5b58fadda6b203a1aa63c
JaydevVadachhak <JaydevVadachhak@gmail.com>
2024-10-14 13:41:35 +0530

    testimonial grid work done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,25 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        strongCyan: 'hsl(171, 66%, 44%)',
-        lightBlue: 'hsl(233, 100%, 69%)',
-        darkGrayishBlue: 'hsl(210, 10%, 33%)',
-        grayishBlue: 'hsl(201, 11%, 66%)',
+        // Clipboard
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
       },
     },
     fontFamily: {
-      // sans: ['Mulish', 'sans-serif'],
-      sans: ['Bai Jamjuree', 'sans-serif'],
-      mono: ['Rokkitt', 'monospace']
+      sans: ['Mulish', 'sans-serif'], // General
+      clipSans: ["Bai Jamjuree", "sans-serif"], // Clipboard
+      tgSans: ["Barlow Semi Condensed", "sans-serif"], // Testimonial Grid
+      mono: ["Rokkitt", "monospace"], // General
     },
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    }
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
   },
   plugins: [],
-}
-
+};
